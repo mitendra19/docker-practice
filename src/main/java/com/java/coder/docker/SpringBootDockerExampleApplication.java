8201e6d6a3a152c2e0af8ea9e5be11dfd6d356ef
mitendra19 <59879017+mitendra19@users.noreply.github.com>
2024-09-02 16:59:01 +0530

    My first commit

---
+++ src/main/java/com/java/coder/docker/SpringBootDockerExampleApplication.java
@@ -0,0 +1,22 @@
+package com.java.coder.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/my-app")
+public class SpringBootDockerExampleApplication {
+
+	@GetMapping("/message")
+	public String getStartMessage() {
+		return "Welcome to My first docker application";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerExampleApplication.class, args);
+	}
+
+}
